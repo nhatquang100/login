7d7e65d4cfcaeb73abe83aef13b3c640cef650e1
nhatquang <vonhatquang100@gmail.com>
2017-11-08 16:00:28 +0700

    khoi tao kho luu tru

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Register success"
+      redirect_to users_path
+    else
+      flash[:success] = "Register failed"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :password, :password_confirmation
+  end
+end
